823bf66f9984988c91a940ad92bb59ddb8fcb761
stuartelimu <stuartelimu@gmail.com>
2020-08-23 16:22:03 +0300

    add filter for products

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 // JavaScript Document
+
+const GHEE = document.querySelector("#ghee");
+const BAGS = document.querySelector("#bags");
+
+filterSelection("all")
+ // Execute the function and show all columns
+function filterSelection(selection) {
+  let x, i;
+  x = document.getElementsByClassName("article");
+  if (selection == "all") selection = "";
+
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(selection) > -1) w3AddClass(x[i], "show");
+  }
+  console.log(selection)
+}
+
+// Show filtered elements
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.querySelector(".btns");
+var btns = btnContainer.getElementsByClassName("btn");
+
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
